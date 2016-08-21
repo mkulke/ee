53ba9377b4f81d027738c90ba1717cd9e33e7ff9
Magnus Kulke <mkulke@gmail.com>
2016-08-21 23:44:43 +0200

    Started porting Tile.

--- src/Main.elm
+++ src/Main.elm
@@ -1,185 +1,222 @@
-module Main (..) where
-
+module Main exposing (..)
 import Tile
-import Train
-import Graphics.Collage exposing (..)
-import Graphics.Element exposing (..)
-import Random
-import StartApp         exposing (start)
-import Task
-import Effects          exposing (Effects, Never)
-import Signal           exposing (Address)
-import Html
-import Time
+import Html exposing (Html, text)
 
+import Html.App as Html
 
--- MAIN
-
-
-app : StartApp.App Model
-app = start
+main = Html.program
   { init = init
   , update = update
   , view = view
-  , inputs = [updateTime]
+  , subscriptions = \_ -> Sub.none
   }
 
+type alias Model = {
+  tile : Tile.Model
+}
 
-main : Signal Html.Html
-main = app.html
-
-
-port tasks : Signal (Task.Task Never ())
-port tasks =
-  app.tasks
-
-
--- INPUTS
-
-
-updateTime : Signal Action
-updateTime =
-  Time.every Time.second
-    |> Signal.map UpdateTime
-
-
--- EFFECTS
-
-
-getTick : Effects Action
-getTick =
-  Effects.tick round
-    |> Effects.map NewTick
-
-
--- MODEL
-
-
-type alias Model =
-  { rails : List (ID, Tile.Model)
-  , train : Train.Model
-  }
-
-
-type alias ID = Int
-
-
-init : (Model, Effects Action)
+init : (Model, Cmd Msg)
 init =
-  (Model [] Train.init, getTick)
+  Model Tile.init ! []
 
 
-tilesGenerator : Int -> Random.Generator (List Tile.Model)
-tilesGenerator count =
-  Random.list count Tile.generator
+-- MSG
 
 
-generateTiles : Int -> Int -> List (ID, Tile.Model)
-generateTiles count seed =
-  Random.generate (tilesGenerator count) (Random.initialSeed seed)
-    |> fst
-    |> List.indexedMap (,)
+type Msg = Tile Tile.Msg
 
-
-size : Int
-size = 10
-
-
--- ACTION
-
-
-type Action
-  = NewTick Int
-  | Tile ID Tile.Action
-  | UpdateTime Time.Time
-
-
--- UPDATE
-
-
-update : Action -> Model -> (Model, Effects Action)
-update action model =
-  case action of
-    NewTick tick -> ( Model (generateTiles (size * size) tick) Train.init
-                    , Effects.none
-                    )
-    Tile id tileAction ->
-      let updateTile ((tileID, tileModel) as tileTuple) =
-            if tileID == id
-            then
-              let (newTileModel, effect) = Tile.update tileAction tileModel
-              in
-                ( (id, newTileModel)
-                , Effects.map (Tile tileID) effect
-                )
-            else (tileTuple, Effects.none)
-          (newTileModel, effects) =
-            model.rails
-              |> List.map updateTile
-              |> List.unzip
-      in
-        ( { model | rails = newTileModel }
-        , Effects.batch effects
-        )
-    UpdateTime _ ->
-      let (_, rail) = model.rails
-            |> List.drop (size * size - model.train)
-            |> List.head
-            |> Maybe.withDefault (0, (Tile.init (0, 0)))
-          { orientation } = rail
-          modifier = case orientation of
-            Tile.North -> 1
-            Tile.South -> 2
-            Tile.West -> 3
-            Tile.East -> 4
-          position = (model.train + modifier) % 100
-          train' = Train.update (Train.Move position) model.train
-      in
-        ({model | train = train'}, Effects.none)
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tile msg' -> { model | tile = Tile.update msg' model.tile } ! []
 
 
 -- VIEW
 
 
-addOffset : (Float, Float) -> (Float, Float)
-addOffset tuple =
-  let half = (toFloat (size - 1) / 2) * (toFloat Tile.size)
-  in
-    ((fst tuple) - half, (snd tuple) - half)
+view : Model -> Html Msg
+view model =
+  Html.map Tile (Tile.view model.tile)
+-- import Tile
+-- import Train
+-- import Graphics.Collage exposing (..)
+-- import Graphics.Element exposing (..)
+-- import Random
+-- import StartApp         exposing (start)
+-- import Task
+-- import Effects          exposing (Effects, Never)
+-- import Signal           exposing (Address)
+-- import Html
+-- import Time
 
 
-view : Address Action -> Model -> Html.Html
-view address model =
-  let zip index = List.map2 (,) (List.repeat size (index * Tile.size))
-      positions = [0..size - 1]
-        |> List.map ((*) Tile.size) -- [0,50,100]
-        |> List.repeat size         -- [[0,50,100],...]
-        |> List.indexedMap zip      -- [[(0,0),(0,50),(0,100)],...]
-        |> List.concat              -- [(0,0),(0,50),(0,100),(50,0)...]
-        |> List.map (\(x, y)-> (toFloat x, toFloat y))
-        |> List.map addOffset
-      tiles = model.rails
-        |> List.map (viewTile address)
-      align forms = forms
-        |> List.map2 (,) positions
-        |> List.map (\(x, y) -> move x y)
-        |> collage (Tile.size * size) (Tile.size * size)
-      rails = tiles
-        |> align
-      train = positions
-        |> List.drop (size * size - model.train)
-        |> List.head
-        |> Maybe.withDefault (0, 0)
-        |> (\position -> move position (Train.view model.train))
-        |> (\form -> form :: [])
-        |> collage (Tile.size * size) (Tile.size * size)
-  in
-    layers [train, rails]
-      |> Html.fromElement
+-- -- MAIN
 
 
-viewTile : Address Action -> (ID, Tile.Model) -> Form
-viewTile address tuple =
-  let (id, tile) = tuple
-  in
-    Tile.view (Signal.forwardTo address (Tile id)) tile
+-- app : StartApp.App Model
+-- app = start
+--   { init = init
+--   , update = update
+--   , view = view
+--   , inputs = [updateTime]
+--   }
+
+
+-- main : Signal Html.Html
+-- main = app.html
+
+
+-- port tasks : Signal (Task.Task Never ())
+-- port tasks =
+--   app.tasks
+
+
+-- -- INPUTS
+
+
+-- updateTime : Signal Action
+-- updateTime =
+--   Time.every Time.second
+--     |> Signal.map UpdateTime
+
+
+-- -- EFFECTS
+
+
+-- getTick : Effects Action
+-- getTick =
+--   Effects.tick round
+--     |> Effects.map NewTick
+
+
+-- -- MODEL
+
+
+-- type alias Model =
+--   { rails : List (ID, Tile.Model)
+--   , train : Train.Model
+--   }
+
+
+-- type alias ID = Int
+
+
+-- init : (Model, Effects Action)
+-- init =
+--   (Model [] Train.init, getTick)
+
+
+-- tilesGenerator : Int -> Random.Generator (List Tile.Model)
+-- tilesGenerator count =
+--   Random.list count Tile.generator
+
+
+-- generateTiles : Int -> Int -> List (ID, Tile.Model)
+-- generateTiles count seed =
+--   Random.generate (tilesGenerator count) (Random.initialSeed seed)
+--     |> fst
+--     |> List.indexedMap (,)
+
+
+-- size : Int
+-- size = 10
+
+
+-- -- ACTION
+
+
+-- type Action
+--   = NewTick Int
+--   | Tile ID Tile.Action
+--   | UpdateTime Time.Time
+
+
+-- -- UPDATE
+
+
+-- update : Action -> Model -> (Model, Effects Action)
+-- update action model =
+--   case action of
+--     NewTick tick -> ( Model (generateTiles (size * size) tick) Train.init
+--                     , Effects.none
+--                     )
+--     Tile id tileAction ->
+--       let updateTile ((tileID, tileModel) as tileTuple) =
+--             if tileID == id
+--             then
+--               let (newTileModel, effect) = Tile.update tileAction tileModel
+--               in
+--                 ( (id, newTileModel)
+--                 , Effects.map (Tile tileID) effect
+--                 )
+--             else (tileTuple, Effects.none)
+--           (newTileModel, effects) =
+--             model.rails
+--               |> List.map updateTile
+--               |> List.unzip
+--       in
+--         ( { model | rails = newTileModel }
+--         , Effects.batch effects
+--         )
+--     UpdateTime _ ->
+--       let (_, rail) = model.rails
+--             |> List.drop (size * size - model.train)
+--             |> List.head
+--             |> Maybe.withDefault (0, (Tile.init (0, 0)))
+--           { orientation } = rail
+--           modifier = case orientation of
+--             Tile.North -> 1
+--             Tile.South -> 2
+--             Tile.West -> 3
+--             Tile.East -> 4
+--           position = (model.train + modifier) % 100
+--           train' = Train.update (Train.Move position) model.train
+--       in
+--         ({model | train = train'}, Effects.none)
+
+
+-- -- VIEW
+
+
+-- addOffset : (Float, Float) -> (Float, Float)
+-- addOffset tuple =
+--   let half = (toFloat (size - 1) / 2) * (toFloat Tile.size)
+--   in
+--     ((fst tuple) - half, (snd tuple) - half)
+
+
+-- view : Address Action -> Model -> Html.Html
+-- view address model =
+--   let zip index = List.map2 (,) (List.repeat size (index * Tile.size))
+--       positions = [0..size - 1]
+--         |> List.map ((*) Tile.size) -- [0,50,100]
+--         |> List.repeat size         -- [[0,50,100],...]
+--         |> List.indexedMap zip      -- [[(0,0),(0,50),(0,100)],...]
+--         |> List.concat              -- [(0,0),(0,50),(0,100),(50,0)...]
+--         |> List.map (\(x, y)-> (toFloat x, toFloat y))
+--         |> List.map addOffset
+--       tiles = model.rails
+--         |> List.map (viewTile address)
+--       align forms = forms
+--         |> List.map2 (,) positions
+--         |> List.map (\(x, y) -> move x y)
+--         |> collage (Tile.size * size) (Tile.size * size)
+--       rails = tiles
+--         |> align
+--       train = positions
+--         |> List.drop (size * size - model.train)
+--         |> List.head
+--         |> Maybe.withDefault (0, 0)
+--         |> (\position -> move position (Train.view model.train))
+--         |> (\form -> form :: [])
+--         |> collage (Tile.size * size) (Tile.size * size)
+--   in
+--     layers [train, rails]
+--       |> Html.fromElement
+
+
+-- viewTile : Address Action -> (ID, Tile.Model) -> Form
+-- viewTile address tuple =
+--   let (id, tile) = tuple
+--   in
+--     Tile.view (Signal.forwardTo address (Tile id)) tile
--- src/Tile.elm
+++ src/Tile.elm
@@ -1,14 +1,8 @@
-module Tile (Model, Action, Orientation(..), init, view, update, size, generator) where
-
-import Random
-import Effects          exposing (Effects)
-import Signal           exposing (Address)
-import Graphics.Element exposing (..)
-import Graphics.Input   exposing (..)
-import Graphics.Collage exposing (..)
-import Color            exposing (..)
-import Time             exposing (Time)
-import Easing           exposing (float, ease, easeOutBounce)
+-- module Tile (Model, Action, Orientation(..), init, view, update, size, generator) where
+module Tile exposing (Model, Msg, init, update, view)
+import Html exposing (Html, div)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
 
 
 -- MODEL
@@ -20,133 +14,216 @@ type Orientation
   | South
   | West
 
-
 type Kind
   = Left
   | Right
   | Straight
 
-
-type alias EvolvingState = Maybe { previous : Time, elapsed: Time }
-
-
-type alias Tile =
+type alias Model =
   { kind : Kind
   , orientation : Orientation
-  , animationState: EvolvingState
   }
 
+init : Model
+init =
+  Model Left North
 
-duration : Time
-duration = Time.second
-
-
-type alias Model = Tile
-
-
-init : (Int, Int) -> Tile
-init pair =
-  let (kindNo, orientationNo) = pair
-      kind =
-        case kindNo of
-          0 -> Left
-          1 -> Right
-          _ -> Straight
-      orientation =
-        case orientationNo of
-          0 -> North
-          1 -> East
-          2 -> South
-          _ -> West
-      -- color = if orientationNo == 0 then Color.blue else Color.lightBlue
+toCSSClasses : Model -> String
+toCSSClasses model =
+  let
+    kind = case model.kind of
+      Left -> "left"
+      Right -> "right"
+      Straight -> "straight"
+    orientation = case model.orientation of
+      North -> "north"
+      East -> "east"
+      South -> "south"
+      West -> "west"
   in
-    Tile kind orientation Nothing
+    "tile " ++ kind ++ " " ++ orientation
+
+rotate : Model -> Model
+rotate model =
+  let orientation' = case model.orientation of
+    North -> East
+    East -> South
+    South -> West
+    West -> North
+  in
+    { model | orientation = orientation' }
 
 
-generator : Random.Generator Tile
-generator =
-  Random.pair (Random.int 0 3) (Random.int 0 3)
-    |> Random.map init
+-- MSG
 
 
--- ACTION
-
-
-type Action
-  = Rotate
-  | Tick Time
+type Msg = Rotate
 
 
 -- UPDATE
 
 
-update : Action -> Model -> (Model, Effects Action)
-update action model =
-  case action of
-    Rotate ->
-      let effect = case model.animationState of
-        Nothing -> Effects.tick Tick
-        Just _ -> Effects.none
-      in
-        (model, effect)
-    Tick time ->
-      let elapsed' = case model.animationState of
-            Nothing -> 0
-            Just {elapsed, previous} -> elapsed + (time - previous)
-          orientation' = case model.orientation of
-            North -> East
-            East -> South
-            South -> West
-            West -> North
-      in
-        if elapsed' > duration
-        then ( { model | animationState = Nothing
-                       , orientation = orientation'
-                       }
-             , Effects.none
-             )
-        else
-          let animationState' = Just { elapsed = elapsed', previous = time }
-          in
-            ( { model | animationState = animationState' }
-            , Effects.tick Tick
-            )
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Rotate -> rotate model
 
 
 -- VIEW
 
 
-size : Int
-size = 50
+view : Model -> Html Msg
+view model =
+  div [ class (toCSSClasses model)
+      , onClick Rotate
+      ] []
+
+-- import Random
+-- import Effects          exposing (Effects)
+-- import Signal           exposing (Address)
+-- import Graphics.Element exposing (..)
+-- import Graphics.Input   exposing (..)
+-- import Graphics.Collage exposing (..)
+-- import Color            exposing (..)
+-- import Time             exposing (Time)
+-- import Easing           exposing (float, ease, easeOutBounce)
+
+-- -- MODEL
 
 
-rotation : Model -> Float
-rotation { orientation, animationState } =
-  let base = case orientation of
-        North -> 0
-        East -> 90
-        South -> 180
-        West -> 270
-      toEased elapsed = ease easeOutBounce float 0 90 duration elapsed
-  in
-    case animationState of
-      Nothing -> base
-      Just { elapsed } -> base + toEased elapsed
+-- type Orientation
+--   = North
+--   | East
+--   | South
+--   | West
 
 
-view : Address Action -> Model -> Form
-view address model =
-  let path { kind } =
-        case kind of
-          Left -> "/img/left.png"
-          Right -> "/img/right.png"
-          Straight -> "/img/straight.png"
-      tileRotate =
-        rotate (rotation model |> degrees)
-      tileImage = path model
-        |> image size size
-        |> Graphics.Input.clickable (Signal.message address Rotate)
-        |> toForm
-        |> tileRotate
-  in
-    tileImage
+-- type Kind
+--   = Left
+--   | Right
+--   | Straight
+
+
+-- type alias EvolvingState = Maybe { previous : Time, elapsed: Time }
+
+
+-- type alias Tile =
+--   { kind : Kind
+--   , orientation : Orientation
+--   , animationState: EvolvingState
+--   }
+
+
+-- duration : Time
+-- duration = Time.second
+
+
+-- type alias Model = Tile
+
+
+-- init : (Int, Int) -> Tile
+-- init pair =
+--   let (kindNo, orientationNo) = pair
+--       kind =
+--         case kindNo of
+--           0 -> Left
+--           1 -> Right
+--           _ -> Straight
+--       orientation =
+--         case orientationNo of
+--           0 -> North
+--           1 -> East
+--           2 -> South
+--           _ -> West
+--       -- color = if orientationNo == 0 then Color.blue else Color.lightBlue
+--   in
+--     Tile kind orientation Nothing
+
+
+-- generator : Random.Generator Tile
+-- generator =
+--   Random.pair (Random.int 0 3) (Random.int 0 3)
+--     |> Random.map init
+
+
+-- -- ACTION
+
+
+-- type Action
+--   = Rotate
+--   | Tick Time
+
+
+-- -- UPDATE
+
+
+-- update : Action -> Model -> (Model, Effects Action)
+-- update action model =
+--   case action of
+--     Rotate ->
+--       let effect = case model.animationState of
+--         Nothing -> Effects.tick Tick
+--         Just _ -> Effects.none
+--       in
+--         (model, effect)
+--     Tick time ->
+--       let elapsed' = case model.animationState of
+--             Nothing -> 0
+--             Just {elapsed, previous} -> elapsed + (time - previous)
+--           orientation' = case model.orientation of
+--             North -> East
+--             East -> South
+--             South -> West
+--             West -> North
+--       in
+--         if elapsed' > duration
+--         then ( { model | animationState = Nothing
+--                        , orientation = orientation'
+--                        }
+--              , Effects.none
+--              )
+--         else
+--           let animationState' = Just { elapsed = elapsed', previous = time }
+--           in
+--             ( { model | animationState = animationState' }
+--             , Effects.tick Tick
+--             )
+
+
+-- -- VIEW
+
+
+-- size : Int
+-- size = 50
+
+
+-- rotation : Model -> Float
+-- rotation { orientation, animationState } =
+--   let base = case orientation of
+--         North -> 0
+--         East -> 90
+--         South -> 180
+--         West -> 270
+--       toEased elapsed = ease easeOutBounce float 0 90 duration elapsed
+--   in
+--     case animationState of
+--       Nothing -> base
+--       Just { elapsed } -> base + toEased elapsed
+
+
+-- view : Address Action -> Model -> Form
+-- view address model =
+--   let path { kind } =
+--         case kind of
+--           Left -> "/img/left.png"
+--           Right -> "/img/right.png"
+--           Straight -> "/img/straight.png"
+--       tileRotate =
+--         rotate (rotation model |> degrees)
+--       tileImage = path model
+--         |> image size size
+--         |> Graphics.Input.clickable (Signal.message address Rotate)
+--         |> toForm
+--         |> tileRotate
+--   in
+--     tileImage
