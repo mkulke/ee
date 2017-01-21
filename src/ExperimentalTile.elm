6e941e86298c8efc3789c3052a2adec60cb39786
Magnus Kulke <mkulke@gmail.com>
2017-01-21 22:58:52 +0100

    Added elm css rotate animation.

--- src/ExperimentalTile.elm
+++ src/ExperimentalTile.elm
@@ -1,7 +1,8 @@
 module ExperimentalTile exposing (Model, Msg, updateTick, init, update, view)
 import Html exposing (Html, div, text)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, style)
 import Html.Events exposing (onClick)
+import Css exposing (asPairs, transform, deg, rotate)
 
 
 -- MODEL
@@ -28,11 +29,12 @@ updateTick diff model =
         then TransitioningSince (time + diff)
         else NotTransitioning
       }
-  -- { model | diff = newDiff }
+
 
 -- MSG
 
 type Msg = Rotate
+
 
 -- UPDATE
 
@@ -44,11 +46,22 @@ update msg model =
       then { model | on = xor model.on True, transitioning = TransitioningSince 0 }
       else model
 
+
 -- VIEW
+
+styles : List Css.Mixin -> Html.Attribute msg
+styles =
+  Css.asPairs >> Html.Attributes.style
+
+rotation transition =
+  case transition of
+    NotTransitioning -> deg 0
+    TransitioningSince time -> deg (360 / 1000 * time)
 
 view : Model -> Html Msg
 view model =
   div [ class "experimentalTile"
+      , styles [ transform (rotate (rotation model.transitioning)) ]
       , class (if model.on then "experimentalOn" else "experimentalOff")
       , onClick Rotate
       ] [ text (toString model.id) ]
