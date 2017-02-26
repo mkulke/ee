c97ac2d1cfcc594e8dea5e127c5fe21e771cdaf2
Magnus Kulke <mkulke@gmail.com>
2017-02-26 11:44:05 +0100

    Crude sin/cos animations, lacking proper abstractions.

--- src/Board.elm
+++ src/Board.elm
@@ -44,6 +44,73 @@ getNewDirections train tile =
     else if trainConnection == otherEnd then Just (otherEnd, oneEnd)
     else Nothing
 
+calculateXTurn : Float -> Float -> Float
+calculateXTurn delta factor =
+  let
+    angle = turns (factor / 4)
+  in
+    delta * cos angle
+
+calculateYTurn : Float -> Float -> Float
+calculateYTurn delta factor =
+  let
+    angle = turns (factor / 4)
+  in
+    delta * sin angle
+
+
+calculateDelta : (Tile.Direction, Tile.Direction) -> Float -> (Float, Float)
+calculateDelta movement progressFactor =
+  let
+    angleOffset = -0.25
+  in
+    case movement of
+      (Tile.North, Tile.South) -> (0, 80 * progressFactor - 40)
+      (Tile.North, Tile.East) ->
+        let
+          angle = turns ((0.75 - (0.25 * progressFactor)) + angleOffset)
+        in
+          ((40 * cos angle) + 40, (40 * sin angle) - 40)
+      (Tile.North, Tile.West) ->
+        let
+          angle = turns (((0.25 * progressFactor) + 0.25) + angleOffset)
+        in
+          ((40 * cos angle) - 40, (40 * sin angle) - 40)
+      (Tile.South, Tile.North) -> (0, -80 * progressFactor + 40)
+      (Tile.South, Tile.East) ->
+        let
+          angle = turns ((0.75 + (0.25 * progressFactor)) + angleOffset)
+        in
+          ((40 * cos angle) + 40, (40 * sin angle) + 40)
+      (Tile.South, Tile.West) ->
+        let
+          angle = turns ((0.25 - (0.25 * progressFactor)) + angleOffset)
+        in
+          ((40 * cos angle) - 40, (40 * sin angle) + 40)
+      (Tile.East, Tile.West) -> (-80 * progressFactor + 40, 0)
+      (Tile.East, Tile.North) ->
+        let
+          angle = turns ((0.5 + (0.25 * progressFactor)) + angleOffset)
+        in
+          ((40 * cos angle) + 40, (40 * sin angle) - 40)
+      (Tile.East, Tile.South) ->
+        let
+          angle = turns ((1.0 - (0.25 * progressFactor)) + angleOffset)
+        in
+          ((40 * cos angle) + 40, (40 * sin angle) + 40)
+      (Tile.West, Tile.East) -> (80 * progressFactor - 40, 0)
+      (Tile.West, Tile.South) ->
+        let
+          angle = turns ((0.25 * progressFactor) + angleOffset)
+        in
+          ((40 * cos angle) + 40 - 80, (40 * sin angle) + 40)
+      (Tile.West, Tile.North) ->
+        let
+          angle = turns ((0.5 - (0.25 * progressFactor)) + angleOffset)
+        in
+          ((40 * cos angle) + 40 - 80, (40 * sin angle) - 40)
+      _ -> (0, 0)
+
 calculateOffsets : Train.Model -> Float -> (Css.Mixin, Css.Mixin)
 calculateOffsets train progressFactor =
   let
@@ -51,15 +118,9 @@ calculateOffsets train progressFactor =
     coordinates = indexToCoordinates index
     xOffset = toFloat (Tuple.first coordinates) * 80
     yOffset = toFloat (Tuple.second coordinates) * 80
-    position = \direction -> case direction of
-      Tile.North -> (xOffset, yOffset - 40)
-      Tile.East -> (xOffset + 40, yOffset)
-      Tile.South -> (xOffset, yOffset + 40)
-      Tile.West -> (xOffset - 40, yOffset)
-    (xStart, yStart) = position from
-    (xEnd, yEnd) = position to
-    y = yStart + ((yEnd - yStart) * progressFactor)
-    x = xStart + ((xEnd - xStart) * progressFactor)
+    (xDelta, yDelta) = calculateDelta (from, to) progressFactor
+    x = xOffset + xDelta
+    y = yOffset + yDelta
   in
     (top (px y), left (px x))
 
